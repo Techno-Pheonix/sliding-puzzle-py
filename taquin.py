917d2a8b186b1082ae42260b650ccd437b5fc6d1
Mohamed Aziz Hafhouf <hafhoufgeek@gmail.com>
2021-04-21 13:33:32 +0100

    commit

--- taquin.py
+++ taquin.py
@@ -7,9 +7,9 @@ mainapp.geometry('1280x720+300+150') #n7ot el settings toul ou 3ordh (+300+150 b
 mainapp.minsize(1280,720)
 mainapp.maxsize(1920,1080)
 #end forced window size
-mainapp.title('Hafhouf Jobs System V1.0')
+mainapp.title('Sliding Puzzle AI Solver')
 mainapp.configure(bg='#00ccff')#background color
-mainapp.iconbitmap(r'hafhouf.ico')
+#mainapp.iconbitmap(r'hafhouf.ico')
 
 class taquin:
   initial_state = [[3,2,7],[8,6,0],[1,5,4]]
@@ -38,7 +38,7 @@ def main():
     # create a job seeker button
     b2=Button(mainframe, text="Job Seeker", height="1", width="30", font=("Verdana", 30),fg='#00ccff',command=lambda:jobmenu(mainapp)).pack(pady = 20)
     mainframe1=LabelFrame(mainapp, bg="#00ccff").pack()
-    mainlabel=Label(mainframe1, text="Developped By : Mohamed Aziz Hafhouf", font=("Verdana", 15), bg='#00ccff',fg='white').pack()
+    mainlabel=Label(mainframe1, text="Developped By : Hafhouf & Welhazi", font=("Verdana", 15), bg='#00ccff',fg='white').pack()
 
     mainframe2=LabelFrame(mainapp, bg="#00ccff").pack()
     mainversion=Label(mainframe2, text="V 1.0", font=("Verdana", 15), bg='#00ccff',fg='white').pack(side="right")
