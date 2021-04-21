701616164bc24ca293bdb36270f07c6986e713a8
MoetezWelhazi <welhazi.moetez@gmail.com>
2021-04-21 14:28:42 +0200

    Update taquin.py

--- taquin.py
+++ taquin.py
@@ -1,2 +1,8 @@
-def main():
-    print("Ahlan    !")
+class taquin:
+  initial_state = [[3,2,7],[8,6,0],[1,5,4]]
+  final_state = [[1,2,3],[8,0,4],[7,6,5]]
+  t=[]
+  def __init__(self):
+    self.t == self.initial_state
+  def est_etat_final():
+    return self.t == self.final_state
