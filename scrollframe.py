6b0d00767df18ff03ecb591e82cad268ed094408
yassineraboudi <yassine-raboudi@outlook.com>
2021-04-26 16:51:00 +0100

    add

---
+++ scrollframe.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("The home screen")
+root.state("zoomed")
+root['bg'] = '#000033'
+
+container = ttk.Frame(root)
+canvas = tk.Canvas(container)
+scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
+scrollable_frame = ttk.Frame(canvas)
+
+scrollable_frame.bind(
+    "<Configure>",
+    lambda e: canvas.configure(
+        scrollregion=canvas.bbox("all")
+    )
+)
+
+canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+canvas.configure(yscrollcommand=scrollbar.set)
+
+for i in range(50):
+    Btn1 = ttk.Button(scrollable_frame, text="Recherche profendeur",
+                      style="BW1.TButton")
+    Btn1.pack(pady=30)
+
+container.pack(fill=tk.BOTH, expand=True)
+
+canvas.pack(side="left", fill="both", expand=True)
+scrollbar.pack(side="right", fill="y")
+
+root.mainloop()
